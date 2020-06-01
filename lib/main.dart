1ce7886f3813ca48599221ef0781b34e2d883659
Sergio Prada <ing.seprab@gmail.com>
2020-05-31 21:09:36 -0500

    Se finaliza la lección de los dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
+String msgDer = "Derecho: ";
+String msgIzq = "Izquierdo: ";
 
 void main() {
   return runApp(
@@ -15,9 +19,56 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  var random = Random();
+
+  Image randomDiceFace()
+  {
+    int randNumber = random.nextInt((6))+1;
+    return Image.asset("images/dice$randNumber.png");
+  }
+  Image leftFace = Image.asset("images/dice1.png");
+  Image rightFace= Image.asset("images/dice1.png");
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftFace = randomDiceFace();
+                });
+                /*Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text(msgIzq + leftDiceNumber.toString())));*/
+              },
+              child: leftFace,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightFace = randomDiceFace();
+                });
+                /*Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text(msgDer + rightDiceNumber.toString())));*/
+              },
+              child: rightFace,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
